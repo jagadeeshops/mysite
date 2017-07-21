a41f03bc7e705c2fe263ca763a82c52c535d98b4
Jagadeesh <karja08@ca.com>
2017-07-21 12:22:10 +0530

    modified travis yml

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -7,6 +7,16 @@ set -e
 : "${SERVER:?SERVER is required and should be non empty. Please set an env variable.}"
 : "${USERNAME:?USERNAME is required and should be non empty. Please set an env variable.}"
 : "${SSHPASS:?SSHPASS is required and should be non empty. Please set an env variable.}"
+: "${TRAVIS_BRANCH:?TRAVIS_BRANCH is required and should be non empty. Please set an env variable.}"
+
+if [ "$TRAVIS_BRANCH" = "master" ]
+then
+	echo "we are in master brach. Attempting to deploy"
+else
+	echo "we are NOT in master brach. So we wont deploy to production."
+	echo "marking deployment as success without deploying actually"
+	exit 0
+fi
 
 
 # this script will be executed on the remote server. 
